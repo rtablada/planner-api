d859000ae2476ce70eb7c49d5b64f4e8b97db4a2
Ryan Tablada <ryan.tablada@gmail.com>
2016-06-12 21:36:29 -0500

    Correct lesson_link

--- web/views/block_view.ex
+++ web/views/block_view.ex
@@ -6,6 +6,6 @@ defmodule Planner.BlockView do
   has_one :lesson, link: :lesson_link
 
   def lesson_link(block, conn) do
-    user_url(conn, :show, block.lesson_id)
+    lesson_url(conn, :show, block.lesson_id)
   end
 end
